ab6adcdd39afe444ee516f3e052b7602098a7828
Nisarga Adhikary <git@nisarga.me>
2023-11-10 02:38:25 +0530

    Create eval.py

---
+++ modules/eval.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import traceback
+import os
+
+SUDO_USER_ID = os.environ['SUDO_USER_ID']
+
+class Eval(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot      
+
+    @commands.command(name="eval")
+    async def _eval(self, ctx, *, code: str):
+        """Evaluate python code (Available only to the developer)"""
+        if ctx.author.id != SUDO_USER_ID:  
+            return
+
+        code = code.strip("```").strip()
+
+        try:
+            result = eval(code)
+            await ctx.send(f"```python\n{result}```")
+        except Exception as e:
+            await ctx.send(f"```python\nError: {e}```")
+            traceback.print_exc()
+
+def setup(bot):
+    bot.add_cog(Eval(bot))
