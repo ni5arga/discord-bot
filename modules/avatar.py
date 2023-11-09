30cd172488c8bd99ad4ebe852a52b3d8cdc97daa
Nisarga Adhikary <git@nisarga.me>
2023-11-10 05:01:07 +0530

    Create avatar.py

---
+++ modules/avatar.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["avatar", "av"])
+    async def get_avatar(self, ctx, user: discord.Member = None):
+        user = user or ctx.author
+        avatar_url = user.avatar_url_as(size=1024)
+
+        embed = discord.Embed(title=f"{user.name}'s Avatar", color=user.color)
+        embed.set_image(url=avatar_url)
+        
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
