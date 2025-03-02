40cddb7e4fd0c1937a9cceb97340dc8e9e949d31
Nisarga <git@nisarga.me>
2025-03-02 23:58:11 +0530

    update repo link

--- modules/commands.py
+++ modules/commands.py
@@ -19,7 +19,7 @@ class commands(commands.Cog):
                 commands_str = "\n".join([f"**{cmd.name}**: {cmd.short_doc}" for cmd in cog_commands])
                 help_embed.add_field(name=cog, value=commands_str[:1024], inline=False)
 
-            help_embed.add_field(name="Repository", value="[GitHub Repository](https://github.com/yourusername/yourrepository)", inline=False)
+            help_embed.add_field(name="Repository", value="[GitHub Repository](https://github.com/ni5arga/discord-bot)", inline=False)
             help_embed.set_footer(text="Use !help <command> for more details on a specific command.")
             await ctx.send(embed=help_embed)
         else:
